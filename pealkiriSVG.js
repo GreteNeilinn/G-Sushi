8652cbe33ef5a749b10c998525695efd791fd8c0
Grete Neilinn <neilinn@ut.ee>
2020-11-30 12:30:45 +0200

    esilehe koodile kommentaarid

--- pealkiriSVG.js
+++ pealkiriSVG.js
@@ -1,7 +1,10 @@
+// Otseselt pole veebilehes kasutamisel,
+// aga kasutasime seda pealkirja svg mõõtmete saamiseks
+
+//leiab elemendid, mis vastavad css selectoriga, milleks praegu on pealkiri
 const pealkiri = document.querySelectorAll("#pealkiri path");
 
-
-// console.log(pealkiri)
 for(let i = 0; i < pealkiri.length; i++){
+    //console.log() == print()
     console.log(`Letter ${i} is ${pealkiri[i].getTotalLength()}`);
 }
