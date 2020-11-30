07d5677de1a66f19a941a06edced57b7a255b0a9
Grete Neilinn <neilinn@ut.ee>
2020-11-30 12:42:15 +0200

    js kommentaar

--- pealkiriSVG.js
+++ pealkiriSVG.js
@@ -1,10 +1,11 @@
 // Otseselt pole veebilehes kasutamisel,
 // aga kasutasime seda pealkirja svg mõõtmete saamiseks
 
-//leiab elemendid, mis vastavad css selectoriga, milleks praegu on pealkiri
+//leiab elemendid, mis vastavad css selectoriga, milleks praegu on pealkirja path-id
 const pealkiri = document.querySelectorAll("#pealkiri path");
 
 for(let i = 0; i < pealkiri.length; i++){
     //console.log() == print()
+    //Saab pealkirjas oleva pathi pikkuse ehk ühe tähe pikkuse ja prindib selle
     console.log(`Letter ${i} is ${pealkiri[i].getTotalLength()}`);
 }
